06090223a33c42383c5bdc0abbba857cfbfc721e
jdeeny <jdeeny@gmail.com>
2018-01-06 05:19:23 -0700

    Start ECS

--- main.lua
+++ main.lua
@@ -3,81 +3,101 @@ require 'math'
 local baton = require 'lib/baton/baton' -- the baton input library https://github.com/tesselode/baton
 local anim8 = require 'lib/anim8/anim8' -- anim8 animation library https://github.com/kikito/anim8
 local cpml = require 'lib/cpml'-- Cirno's Perfect Math Library https://github.com/excessive/cpml (Docs: http://excessive.github.io/cpml/)
+local HC = require 'lib/HC' -- General purpose collision detection library for the use with LÖVE. https://github.com/vrld/HC (Docs: http://hc.readthedocs.org)
+--local class = require 'lib/middleclass/middleclass' -- OOP https://github.com/kikito/middleclass
+local lovetoys = require('lib/lovetoys/lovetoys') -- Entity-Componet System https://github.com/Lovetoys/lovetoys
+lovetoys.initialize({
+    globals = true,
+    debug = true
+})
+
+
 --require 'lib/autobatch/autobatch'                 -- autobatch automatic SpriteBatch https://github.com/rxi/autobatch
--- OOP https://github.com/kikito/middleclass
 -- nice scaling for pixel graphics (might be fixed in git version?) https://github.com/SystemLogoff/lovePixel
 -- interesting text library that allows control per letter https://github.com/mzrinsky/popo https://github.com/EntranceJew/popo
 -- neural net that generates text that looks like source data https://github.com/karpathy/char-rnn
 -- https://github.com/tastyminerals/coherent-text-generation-limited
 -- sound synth lib https://github.com/vrld/Moan
 
+
 local input = baton.new {
   controls = {
-    left = {'key:left', 'axis:leftx-', 'button:dpleft'},
-    right = {'key:right', 'axis:leftx+', 'button:dpright'},
-    up = {'key:up', 'axis:lefty-', 'button:dpup'},
-    down = {'key:down', 'axis:lefty+', 'button:dpdown'},
-    accel = {'key:x', 'button:a'},
-    brake = {'key:c', 'button:b'}
+    moveleft = {'key:a', 'axis:leftx-'},
+    moveright = {'key:d', 'axis:leftx+'},
+    moveup = {'key:w', 'axis:lefty-'},
+    movedown = {'key:s', 'axis:lefty+'},
+
+    aimleft = {'key:left', 'axis:rightx-'},
+    aimright = {'key:right', 'axis:rightx+'},
+    aimup = {'key:up', 'axis:righty-'},
+    aimdown = {'key:down', 'axis:righty+'},
+
+    --fire = {'key:space', 'axis:triggerright', 'button:a'},
   },
   pairs = {
-    move = {'left', 'right', 'up', 'down'}
+    move = {'moveleft', 'moveright', 'moveup', 'movedown'},
+    aim = {'aimleft', 'aimright', 'aimup', 'aimdown'},
   },
   joystick = love.joystick.getJoysticks()[1],
 }
 
-local playerCar = {}
-local road = {}
-local camera = {}
 
+local Fruit = class('Fruit') -- 'Fruit' is the class' name
 
-
+local dude = {}
 
 function love.load()
-  playerCar.x = 100
-  playerCar.y = 100
-  playerCar.anim_image = love.graphics.newImage('assets/sprites/redcar.png')
-  playerCar.anim_grid = anim8.newGrid(96, 64, playerCar.anim_image:getWidth(), playerCar.anim_image:getHeight())
-  playerCar.animation = anim8.newAnimation(playerCar.anim_grid('1-2', 1), 0.4)
+  engine = Engine()
+  dude.x = 100
+  dude.y = 100
+  dude.anim_image = love.graphics.newImage('assets/sprites/dude.png')
+  dude.anim_grid = anim8.newGrid(75, 52, dude.anim_image:getWidth(), dude.anim_image:getHeight())
+  dude.animation = anim8.newAnimation(dude.anim_grid(1, 1), 0.4)
 end
 
 
+local function shoot()
+
+end
 
 local function update_car(dt)
 
   local x, y = input:get 'move'
 
   if x < -0.2 then
-    playerCar.x = playerCar.x - 1
+    dude.x = dude.x - 1
   elseif x > 0.2 then
-    playerCar.x = playerCar.x + 1
+    dude.x = dude.x + 1
   end
 
   if y < -0.2 then
-    playerCar.y = playerCar.y - 1
+    dude.y = dude.y - 1
   elseif y > 0.2 then
-    playerCar.y = playerCar.y + 1
+    dude.y = dude.y + 1
   end
 
 --[[
   if input:pressed 'accel' then
-    playerCar.speed = playerCar.speed + 10 * dt
+    dude.speed = dude.speed + 10 * dt
   elseif input:pressed 'brake' then
-    playerCar.speed = playerCar.speed - 10 * dt
+    dude.speed = dude.speed - 10 * dt
   else
-    playerCar.speed = playerCar.speed - 1 * dt
+    dude.speed = dude.speed - 1 * dt
   end
 --]]
-  playerCar.x = cpml.utils.clamp(playerCar.x, 0, love.graphics.getWidth())
-  playerCar.y = cpml.utils.clamp(playerCar.y, 0, love.graphics.getHeight())
+  dude.x = cpml.utils.clamp(dude.x, 0, love.graphics.getWidth())
+  dude.y = cpml.utils.clamp(dude.y, 0, love.graphics.getHeight())
 
 end
 
 
 function love.update(dt)
 	input:update()
-  update_car(dt)
-  playerCar.animation:update(dt)
+  engine:update(dt)
+  --world:update(dt)
+
+--  update_car(dt)
+--  dude.animation:update(dt)
 end
 
 
@@ -88,6 +108,7 @@ end
 
 
 function love.draw()
-  draw_road()
-  playerCar.animation:draw(playerCar.anim_image, playerCar.x, playerCar.y)
+  engine:draw()
+--  draw_road()
+--  dude.animation:draw(dude.anim_image, dude.x, dude.y)
 end
