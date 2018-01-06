c1d9befa8e49ba66f707ae3aaf217391ce9a85fa
ontoclasm <yokomeshi@gmail.com>
2018-01-06 11:03:36 -0600

    requires.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,4 @@
-require 'math'
-
-local baton = require 'lib/baton/baton' -- the baton input library https://github.com/tesselode/baton
-local anim8 = require 'lib/anim8/anim8' -- anim8 animation library https://github.com/kikito/anim8
-local cpml = require 'lib/cpml'-- Cirno's Perfect Math Library https://github.com/excessive/cpml (Docs: http://excessive.github.io/cpml/)
---require 'lib/autobatch/autobatch'                 -- autobatch automatic SpriteBatch https://github.com/rxi/autobatch
--- OOP https://github.com/kikito/middleclass
--- nice scaling for pixel graphics (might be fixed in git version?) https://github.com/SystemLogoff/lovePixel
--- interesting text library that allows control per letter https://github.com/mzrinsky/popo https://github.com/EntranceJew/popo
--- neural net that generates text that looks like source data https://github.com/karpathy/char-rnn
--- https://github.com/tastyminerals/coherent-text-generation-limited
--- sound synth lib https://github.com/vrld/Moan
+require "requires"
 
 local input = baton.new {
   controls = {
