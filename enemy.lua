7557858cc7b4bd1323b615b72c6ba0c4934f6d14
jdeeny <jdeeny@gmail.com>
2018-01-11 01:16:09 -0700

    Enemies seek out the player like morons

--- enemy.lua
+++ enemy.lua
@@ -1,20 +1,41 @@
 local enemy = class('enemy', mob)
 
+function enemy.init()
+    --enemy.next_attack = game_time
+    --enemy.wake_time = game_time
+end
+
 function enemy:update(dt)
+    if self.next_attack == nil then
+      self.next_attack = game_time
+    end
+
+    playerloc = cpml.vec2(player.x, player.y)
+    meloc = cpml.vec2(self.x, self.y)
+    if playerloc:dist(meloc) <= 100 then
+      if game_time >= self.next_attack or 0 then
+        -- attack
+        player:be_attacked(10)
+        self.next_attack = game_time + .4 + love.math.random(.1)
+      end
+    end
+
   if game_time >= self.wake_time then
     -- stagger around randomly, i guess
-    local angle = love.math.random() * math.pi * 2
+--   angle = love.math.random() * math.pi * 2
+-- if we can, move towards player
+angle = playerloc:angle_to(meloc)
 
     self.dx = math.cos(angle) * self.speed
     self.dy = math.sin(angle) * self.speed
-
-    -- face the direction we're going
     self.rot = angle
 
-    self.wake_time = game_time + love.math.random() * 3
+    -- face the direction we're going
 
-    self:run_ai()
+    self.wake_time = game_time + love.math.random() * 1
   end
+
+  self:run_ai()
 
   self:update_position(dt)
 end
@@ -30,16 +51,8 @@ end
 
 function enemy:run_ai()
     -- if we are adjacent to player, attack
-    playerloc = cpml.vec2(player.x, player.y)
-    meloc = cpml.vec2(self.x, self.y)
-    if playerloc:dist(meloc) < 100 then
-        -- attack
-        player:be_attacked(10)
-        self.next_attack = game_time + .4 + love.math.random(.1)
-    end
     -- else try to pathfind to player
 
-    -- if we can, move towards player
 end
 
 
