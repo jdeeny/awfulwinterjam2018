4d6883c8830db4d98024696cc7c7e6f9268104d2
TheRealPickle <therealpickle@gmail.com>
2018-01-11 21:33:06 -0700

    added lovelightning submodule fixed no shooty

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ function love.load()
   player.x = 300
   player.y = 300
   player.rot = 0
-  --player:equip('weapon', weapon.ProjectileGun:new())
+  player:equip('weapon', weapon.ProjectileGun:new())
 
   player_input, menu_input = controls.init()
   player_input.deadband = 0.2
