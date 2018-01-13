3c4aac9b071222583272e1df6f6d335d67d15829
jdeeny <jdeeny@gmail.com>
2018-01-12 17:40:57 -0700

    spin faster

--- animation.lua
+++ animation.lua
@@ -6,8 +6,8 @@ function animation.init()
   animation.tesla_run_se = anim8.newAnimation(tesla_se_grid('1-8',1), 0.1)
 
   local gear_grid = anim8.newGrid(64, 96, image.gear:getWidth(), image.gear:getHeight(), 0, 0, 0)
-  animation.gear_spin_cw = anim8.newAnimation(gear_grid('1-6',1), 0.2)
-  animation.gear_spin_ccw = anim8.newAnimation(gear_grid('6-1',1), 0.2)
+  animation.gear_spin_cw = anim8.newAnimation(gear_grid('1-6',1), 0.12)
+  animation.gear_spin_ccw = anim8.newAnimation(gear_grid('6-1',1), 0.12)
 end
 
 return animation
