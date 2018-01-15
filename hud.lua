24c9c13ba8f3e35c7285a6c6ba729ab589c9f510
ontoclasm <yokomeshi@gmail.com>
2018-01-15 14:45:04 -0600

    Fix arrow drawing

--- hud.lua
+++ hud.lua
@@ -12,27 +12,33 @@ function hud.draw_arrows()
 	local r,g,b,a
 	local yc = image['arrow']:getHeight()/2
 	local xc = image['arrow']:getWidth()/2
-	
+
 	r,g,b,a = love.graphics.getColor()
 	love.graphics.setColor(r,g,b,hud.arrow_alpha)
-	
+
 	-- draw all arrows
+	local dir
+	local distance = math.max(100, math.min(window.w/2 - 100, window.h/2 - 100))
 	if hud.arrows['up'] then
-		love.graphics.draw(image['arrow'], (window.w/2), 0, 0, 1, 1, xc, 0)
+		dir = math.atan2(-4 * TILESIZE - player.y, (current_room:pixel_width() / 2) - player.x)
+		love.graphics.draw(image['arrow'], (window.w/2) + math.cos(dir) * (distance), (window.h/2) + math.sin(dir) * (distance), dir, 1, 1, xc, yc)
 	end
-	
+
 	if hud.arrows['right'] then
-		love.graphics.draw(image['arrow'], (window.w - xc), (window.h/2), (math.pi * 0.5), 1, 1, xc, yc)
+		dir = math.atan2((current_room:pixel_height() / 2) - player.y, current_room:pixel_width() + 4 * TILESIZE - player.x)
+		love.graphics.draw(image['arrow'], (window.w/2) + math.cos(dir) * (distance), (window.h/2) + math.sin(dir) * (distance), dir, 1, 1, xc, yc)
 	end
-	
+
 	if hud.arrows['left'] then
-		love.graphics.draw(image['arrow'], xc, (window.h/2), (math.pi * 1.5), 1, 1, xc, yc)
+		dir = math.atan2((current_room:pixel_height() / 2) - player.y, -4 * TILESIZE - player.x)
+		love.graphics.draw(image['arrow'], (window.w/2) + math.cos(dir) * (distance), (window.h/2) + math.sin(dir) * (distance), dir, 1, 1, xc, yc)
 	end
-	
+
 	if hud.arrows['down'] then
-		love.graphics.draw(image['arrow'], (window.w/2), (window.h - yc), (math.pi * 1.0), 1, 1, xc, yc)
+		dir = math.atan2(current_room.pixel_height() + 4 * TILESIZE - player.y, (current_room:pixel_width() / 2) - player.x)
+		love.graphics.draw(image['arrow'], (window.w/2) + math.cos(dir) * (distance), (window.h/2) + math.sin(dir) * (distance), dir, 1, 1, xc, yc)
 	end
-	
+
 	-- restore previous draw settings
 	love.graphics.setColor(r,g,b,a)
 end
@@ -45,65 +51,63 @@ function hud.flash_arrows()
 end
 
 
-function hud.draw() 
-	
-	-- hud icons are fixed-size 
+function hud.draw()
+
+	-- hud icons are fixed-size
 	local iconWidth = 80
 	local iconHeight = 80
 	local iconSeparation = 20
-	
+
 	-- draw arrows if room is unlocked
-	if current_room.cleared  then
-		if true then -- DBG
-			if next(hud.arrows) == nil then 
-				-- not currently flashing
-				if current_room.exits.north then
-					hud.arrows.up = true
-				end
-				if current_room.exits.east then
-					hud.arrows.right = true
-				end
-				-- start flasher
-				hud:flash_arrows() -- comment out to disable flash
+	if current_room.cleared then
+		if next(hud.arrows) == nil then
+			-- not currently flashing
+			if current_room.exits.north then
+				hud.arrows.up = true
 			end
-		else 
-			if next(hud.arrows) then
-				hud.arrow_tween:stop() -- comment out if flash disabled
+			if current_room.exits.east then
+				hud.arrows.right = true
 			end
-			hud.arrows = {}
+			-- start flasher
+			hud:flash_arrows() -- comment out to disable flash
 		end
+	else
+		if next(hud.arrows) then
+			hud.arrow_tween:stop() -- comment out if flash disabled
+		end
+		hud.arrows = {}
 	end
-	
-	if next(hud.arrows) then  
+
+	if next(hud.arrows) then
 		hud:draw_arrows()
 	end
-		
-	
+
+
 	love.graphics.setFont(hud.font)
-	
+
 	-- draw timer in upper right
 	timer.draw((window.w-275),0)
-	
+
 	-- draw player HP in lower right
 	love.graphics.print(player.hp, (window.w - 80), (window.h - 80))
 
 	-- draw weapon icons
 	local iconOffset = iconSeparation
-	
+
 	selected_weapon = player.weapons[player.weapon]
 	love.graphics.draw(image[selected_weapon.icon],iconOffset,(window.h - 100), 0, 1, 1, 0, 0)
-	
+
 	iconOffset = iconOffset + iconWidth
-	
+
 	-- Other player weapons drawn at half size
 	for i,w in ipairs(player.weapons) do
 		if w ~= selected_weapon then
-			iconOffset = iconOffset + iconSeparation 
+			iconOffset = iconOffset + iconSeparation
 			love.graphics.draw(image[w.icon],iconOffset,(window.h - 100)+iconHeight/2,0,0.5,0.5,0,0)
 			iconOffset = iconOffset + iconWidth/2
 		end
 	end
-	
+
 end
 
 
