0c11cac68fbcb87ec4d3e481752064a3bd443e0c
jdeeny <jdeeny@gmail.com>
2018-01-14 17:29:55 -0700

    Remove random bolt spawns

--- electricity/boltmanager.lua
+++ electricity/boltmanager.lua
@@ -19,13 +19,13 @@ function BoltManager:add(source, target, intensity, time)
 end
 
 function BoltManager:update(dt)
-  if math.random() < (1.0 * dt) then
+  --[[if math.random() < (1.0 * dt) then
     print("added")
     self:add(cpml.vec2.new(math.random() * 500, math.random() * 500),
               cpml.vec2.new(math.random() * 500, math.random() * 500),
               math.random(),
               math.random() * 1 + 1)
-  end
+  end]]
 
   for id, b in pairs(self.bolts) do
     if b.timeout < game_time then
