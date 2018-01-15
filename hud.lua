7a7280dbe9619daeb3ce08672a4670d016b44013
Michael Winterstein <parzival@quirksand.net>
2018-01-14 22:48:14 -0800

    Add arrows to hud upon room completion

--- hud.lua
+++ hud.lua
@@ -3,9 +3,59 @@ local hud = {}
 hud.font = love.graphics.newFont('assets/fonts/babes-in-toyland-nf/BabesInToylandNF.ttf', 50)
 
 
+hud.arrows = {}
+hud.arrow_alpha = 128
+
+function hud.draw_arrows()
+	local r,g,b,a
+	local yc = image['arrow']:getHeight()/2
+	local xc = image['arrow']:getWidth()/2
+	
+	print("drawin' arrows!") -- DBG
+	print(hud.arrows['up']) -- DBG
+	
+	--r,g,b,a = love.graphics.getColor()
+	--love.graphics.setColor(r,g,b,hud.arrow_alpha)
+	
+	-- draw all arrows
+	if hud.arrows['up'] then
+		love.graphics.draw(image['arrow'], (window.w/2), 0, 0, 1, 1, xc, 0)
+	end
+	
+	if hud.arrows['right'] then
+		love.graphics.draw(image['arrow'], (window.w - 40), (window.h/2), (math.pi * 0.5), 1, 1, 0, yc)
+	end
+	
+	-- restore previous draw settings
+	--love.graphics.setColor(r,g,b,a)
+end
+
+
 function hud.draw() 
 	
+	-- hud icons are fixed-size 
+	local iconWidth = 80
+	local iconHeight = 80
+	local iconSeparation = 20
 	
+	-- draw arrows if room is unlocked
+	if current_room.done_state == 'coda' then
+		if next(hud.arrows) == nil then
+			if current_room.exits.north then
+				hud.arrows.up = true
+			end
+			if current_room.exits.east then
+				hud.arrows.right = true
+			end
+		end
+	else 
+		hud.arrows = {}
+	end
+	
+	
+	if next(hud.arrows) then  
+		hud:draw_arrows()
+	end
 		
 	
 	love.graphics.setFont(hud.font)
@@ -17,9 +67,7 @@ function hud.draw()
 	love.graphics.print(player.hp, (window.w - 80), (window.h - 80))
 
 	-- draw weapon icons
-	local iconWidth = 80
-	local iconHeight = 80
-	local iconSeparation = 20
+	
 	local iconOffset = iconSeparation
 	
 	selected_weapon = player.weapons[player.weapon]
