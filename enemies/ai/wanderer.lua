2e335338ce00a4cbbbb1d02dd560ebfb61a1b736
jdeeny <jdeeny@gmail.com>
2018-01-16 18:59:07 -0700

    Make random wanderer

--- enemies/ai/wanderer.lua
+++ enemies/ai/wanderer.lua
@@ -3,12 +3,12 @@ local Wanderer = class("Wanderer", Ai)
 function Wanderer:update(dt)
   Ai.update(self, dt) -- call superclass constructor
   if not self.entity.dx or game_time >= self.entity.wake_time then
-    angle = math.atan2(player.y - self.entity.y, player.x - self.entity.x)
+    angle = 3.14159 * 2 * math.random()
 
     self.entity.dx = math.cos(angle) * self.entity.speed
     self.entity.dy = math.sin(angle) * self.entity.speed
 
-    self.entity.wake_time = game_time + love.math.random() * 1
+    self.entity.wake_time = game_time + love.math.random() * 3 + love.math.random()
   end
 end
 
