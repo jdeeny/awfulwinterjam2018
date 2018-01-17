778251312a1aacc830612f6ef302cc0ae3ab11ca
jdeeny <jdeeny@gmail.com>
2018-01-17 10:55:55 -0700

    Animate sparks

--- spark.lua
+++ spark.lua
@@ -1,6 +1,9 @@
 local spark = class('spark')
 
 function spark:update(dt)
+  if self.animation then
+    self.animation.update(dt)
+  end
   if self.duration:finished() then
     self:die()
   else
@@ -23,8 +26,13 @@ end
 
 function spark:draw()
   love.graphics.setColor(self.color.r, self.color.g, self.color.b, 255 - 255 * self.duration:t())
-  love.graphics.draw(image[self.sprite], camera.view_x(self), camera.view_y(self),
-    self.r or 0, self.sx or 1, self.sy or 1, self.sprite_hwidth, self.sprite_hheight)
+  if self.animation == nil then
+    love.graphics.draw(image[self.sprite], camera.view_x(self), camera.view_y(self), self.r or 0, 1, 1,
+      image[self.sprite]:getWidth()/2, image[self.sprite]:getHeight()/2)
+  else
+    self.animation:draw(image[self.sprite], camera.view_x(self), camera.view_y(self), self.r or 0, 1, 1,
+      64/2, 72/2) -- SIZE!?!
+  end
   love.graphics.setColor(255, 255, 255, 255)
 end
 
