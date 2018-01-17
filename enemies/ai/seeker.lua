ff484f3e07deb893d1bb8bc8bad837a6eb2280b4
ontoclasm <yokomeshi@gmail.com>
2018-01-17 01:29:20 -0600

    Use pathfinding to have enemies seek out the player

--- enemies/ai/seeker.lua
+++ enemies/ai/seeker.lua
@@ -2,8 +2,15 @@ local Seeker = class("Seeker", Ai)
 
 function Seeker:update(dt)
   Ai.update(self, dt) -- call superclass constructor
-  if not self.entity.dx or game_time >= self.entity.wake_time then
-    angle = math.atan2(player.y - self.entity.y, player.x - self.entity.x)
+  local need_goal = not self.goal_x or math.abs(self.entity.x - self.goal_x) + math.abs(self.entity.y - self.goal_y) < 16
+  if not self.entity.dx or game_time >= self.entity.wake_time or need_goal then
+  	if need_goal then
+  		local gx, gy = room.pos_to_grid(self.entity.x), room.pos_to_grid(self.entity.y)
+  		local dx, dy = pathfinder.hill_climb(gx, gy)
+  		self.goal_x = TILESIZE * (gx + dx + 0.5)
+  		self.goal_y = TILESIZE * (gy + dy + 0.5)
+  	end
+    angle = math.atan2(self.goal_y - self.entity.y, self.goal_x - self.entity.x)
 
     self.entity.dx = math.cos(angle) * self.entity.speed
     self.entity.dy = math.sin(angle) * self.entity.speed
