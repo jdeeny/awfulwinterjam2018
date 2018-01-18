ce8b13b3a535e77d8f15a17699d2018913cf5112
ontoclasm <yokomeshi@gmail.com>
2018-01-17 22:04:49 -0600

    Place stuff correctly after it comes in a door

--- dungeon.lua
+++ dungeon.lua
@@ -19,15 +19,15 @@ function dungeon.move_to_room(rx, ry, from_dir)
   -- we should only ever come from south or west, but still
   if from_dir == "north" then
     player.x = current_level:pixel_width() / 2
-    player.y = 3 * TILESIZE - player.speed / 2
+    player.y = 2.5 * TILESIZE - player.speed / 2
   elseif from_dir == "south" then
     player.x = current_level:pixel_width() / 2
-    player.y = current_level:pixel_height() - 3 * TILESIZE + player.speed / 2
+    player.y = current_level:pixel_height() - 2.5 * TILESIZE + player.speed / 2
   elseif from_dir == "west" then
-    player.x = 3 * TILESIZE - player.speed / 2
+    player.x = 2.5 * TILESIZE - player.speed / 2
     player.y = current_level:pixel_height() / 2
   elseif from_dir == "east" then
-    player.x = current_level:pixel_width() - 3 * TILESIZE + player.speed / 2
+    player.x = current_level:pixel_width() - 2.5 * TILESIZE + player.speed / 2
     player.y = current_level:pixel_height() / 2
   else
     love.errhand("bad room from_dir")
