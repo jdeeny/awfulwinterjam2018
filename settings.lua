8f75bb52fa663a34be498dba6026aef26e6a0148
ontoclasm <yokomeshi@gmail.com>
2018-01-18 01:24:50 -0600

    Lower default volume (a lot)

--- settings.lua
+++ settings.lua
@@ -1,8 +1,8 @@
-local default_settings = { 
+local default_settings = {
   -- These should be modified with the correct variables/names as required
   game_speed = 1.0,
   start_dungeon = 1,
-  volume = 100,
+  volume = 50,
   max_unlocked_dungeon = 1,
   gfx_mode = 'windowed',
   window_width =  window.w,
@@ -28,16 +28,16 @@ settings = default_settings
 
  -- bitser won't serialize functions, so these need to be global
 
-function init_settings() 
-	AudioManager:setMasterVolume(settings.volume/10)
+function init_settings()
+	AudioManager:setMasterVolume(settings.volume/200)
 	for i, s in ipairs({0.3,0.75,1.0,1.25}) do    -- Game speed
 		if s == settings.game_speed then
 			allowed_options[2]:setIndex(i)
 		end
-	end  
+	end
 end
 
-function save_settings()    
+function save_settings()
 	bitser.dumpLoveFile('savedata', settings)
 end
 
