1adc8708c62cd6deb46a72701022c0ae90fa5256
TheRealPickle <therealpickle@gmail.com>
2018-01-18 23:28:31 -0700

    adding more upgrade types

--- enemies/enemy_data.lua
+++ enemies/enemy_data.lua
@@ -51,9 +51,11 @@ enemy_data["schmuck"] =
   value = 1,
   touch_damage = 10,
   personality = 'Wanderer',
-  drop_items = {{chance=0.1,item="max_ammo_increase"},
-                {chance=0.1,item="damage_mult"},
-                {chance=0.5,item="health_pack"},},
+  drop_items = {{chance=0.05,item="max_ammo_increase"},
+                {chance=0.05,item="max_health_increase"},
+                {chance=0.05,item="damage_mult"},
+                {chance=0.05,item="charge_rate_mult"},
+                {chance=0.25,item="health_pack"},},
 }
 
 enemy_data["fodder"] =
@@ -67,9 +69,11 @@ enemy_data["fodder"] =
   value = 0.5,
   touch_damage = 10,
   personality = 'Seeker',
-  drop_items = {{chance=0.1,item="max_ammo_increase"},
-                {chance=0.1,item="damage_mult"},
-                {chance=0.5,item="health_pack"},},
+  drop_items = {{chance=0.05,item="max_ammo_increase"},
+                {chance=0.05,item="max_health_increase"},
+                {chance=0.05,item="damage_mult"},
+                {chance=0.05,item="charge_rate_mult"},
+                {chance=0.25,item="health_pack"},},
 }
 
 enemy_data["rifledude"] =
@@ -86,9 +90,11 @@ enemy_data["rifledude"] =
   weapon_type = weapon.ProjectileGun,
   projectile_type = 'enemybullet',
   personality = 'Rifleman',
-  drop_items = {{chance=0.1,item="max_ammo_increase"},
-                {chance=0.1,item="damage_mult"},
-                {chance=0.5,item="health_pack"},},
+  drop_items = {{chance=0.05,item="max_ammo_increase"},
+                {chance=0.05,item="max_health_increase"},
+                {chance=0.05,item="damage_mult"},
+                {chance=0.05,item="charge_rate_mult"},
+                {chance=0.25,item="health_pack"},},
 }
 
 enemy_data["remotedude_red"] =
--- items/item_data.lua
+++ items/item_data.lua
@@ -42,7 +42,14 @@ item_data['damage_mult'] =
                 player.equipped_items['weapon'].damage = 
                     player.equipped_items['weapon'].damage * self.damage_mult
             end
+            -- special bonus for lightnning gun
+            if owner.equipped_items['weapon'].chain_targets then
+                player.equipped_items['weapon'].chain_targets = 
+                    player.equipped_items['weapon'].chain_targets + 1
+            end
+
             item_data.destroy(self.id)
+
         end,
     sprite = image.patent_icon,
 }
@@ -55,6 +62,33 @@ item_data['max_ammo_increase'] =
             if owner.equipped_items['weapon'].max_ammo then
                 player.equipped_items['weapon'].max_ammo = 
                     player.equipped_items['weapon'].max_ammo + self.ammo_increase
+            end
+            item_data.destroy(self.id)
+        end,
+    sprite = image.patent_icon,
+}
+
+item_data['max_health_increase'] = 
+{
+    radius = 30,
+    hp_increase = 50,
+    pick_up = function(self, owner)
+            if owner.max_hp then
+                player.max_hp = player.max_hp + self.hp_increase
+            end
+            item_data.destroy(self.id)
+        end,
+    sprite = image.patent_icon,
+}
+
+item_data['charge_rate_mult'] = 
+{
+    radius = 30,
+    rate_mult = 1.25,
+    pick_up = function(self, owner)
+            if owner.equipped_items['weapon'].charge_rate then
+                player.equipped_items['weapon'].charge_rate = 
+                    player.equipped_items['weapon'].charge_rate + self.rate_mult
             end
             item_data.destroy(self.id)
         end,
