30de75e4a066f6a881c349fa878463ef9b2360ef
Michael Winterstein <parzival@quirksand.net>
2018-01-19 19:33:33 -0800

    Room files can be restricted in a dungeon

--- gamestage.lua
+++ gamestage.lua
@@ -24,7 +24,8 @@ stages[3] = {
 	dungeon_y = 2,
 	film_title = "Time\nto\nTest",
 	film_music = nil,
-	film_music_start = nil
+	film_music_start = nil,
+	room_files = {1,3,5,9},  -- indexed rooms in file_io
 }
 gamestage.stages = stages
 gamestage.current_stage = 0
@@ -46,7 +47,7 @@ function gamestage.setup_next(forced)
 	film.set_music(next_stage.film_music, next_stage.film_music_start)
 	
     current_dungeon = dungeon:new()
-    current_dungeon:init(next_stage.dungeon_x, next_stage.dungeon_y)
+    current_dungeon:init(next_stage.dungeon_x, next_stage.dungeon_y, next_stage.room_files)
     current_dungeon:setup_main()
 	
 end
