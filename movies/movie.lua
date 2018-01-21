45f18b5e3735c2a23a567ee6e950b1a271d856f1
TheRealPickle <therealpickle@gmail.com>
2018-01-20 23:56:09 -0700

    reorg on movies

---
+++ movies/movie.lua
@@ -0,0 +1,18 @@
+Movie = class("Movie")
+
+
+function Movie:initialize(sequence_steps, dungeon)
+    self.steps = sequence_steps
+
+end
+
+function Movie:update(dt)
+
+end
+
+function Movie:draw()
+
+end
+
+
+return Movie
--- states/movie_a.lua
+++ states/movie_a.lua
@@ -1,92 +1,16 @@
-Intertitle = require "states/intertitle"
+local Intertitle = require "movies/intertitle"
+local Movie = require "movies/movie"
+
+local sequence = require "movies/sequence"
+
+local SequenceStep = sequence.SequenceStep
+local IntertitleStep = sequence.IntertitleStep
 
 local movie_a = {
   music = {track="figleaf", volume=1, offset=27},
 }
 
--------------------------------------------------------------------------------
-local SequenceStep = class("SequenceStep")
 
-function SequenceStep:initialize( seq_table )
-  self.type = seq_table.type
-  self.run_time = seq_table.run_time or 9999
-  self._start = seq_table.start
-  self._stop = seq_table.stop
-  self._update = seq_table.update
-  self._draw = seq_table.draw
-end
-
-function SequenceStep:start( ... )
-  self.start_time = love.timer.getTime()
-  self._started = true
-  self._running = true
-  
-  if self._start then
-    self:_start()
-  end
-end
-
-function SequenceStep:update(dt)
-  if self._running then
-    if love.timer.getTime() > (self.start_time + self.run_time) then
-      self._running = false
-    else
-      if self._update then
-        self:_update(dt)
-      end
-      if self.type == 'animation' then
-        movie_a._update_level(dt)
-      end
-    end
-  end
-end
-
-function SequenceStep:stop( ... )
-  if self._running then
-    self._running = false
-    if self._stop then
-      self:_stop()
-    end
-  end
-end
-
-function SequenceStep:draw( ... )
-  if self._draw and self._running then
-    self:_draw()
-  end
-
-  if self.type == 'animation' then
-    movie_a._draw_level()
-  end
-end
-
-function SequenceStep:is_finished( ... )
-  if not self._started then
-    return false 
-  end
-  return not self._running
-end
-
--------------------------------------------------------------------------------
-IntertitleStep = class("IntertitleStep",SequenceStep)
-
-function IntertitleStep:initialize(text, duration)
-  self._text = text
-  self._duration = duration
-  local startfunc = function(self)
-    self.intertitle = Intertitle:new(self._text,self._duration)
-  end
-  local updatefunc = function(self, dt)
-    if self.intertitle:complete() then
-        self:stop()
-      end
-  end
-  local drawfunc = function(self)
-    self.intertitle:draw()
-  end
-  SequenceStep.initialize(self, {start=startfunc, update=updatefunc,
-    draw=drawfunc, type='intertitle'})
-end
 
 -------------------------------------------------------------------------------
 -- type, start, update, stop, draw, run_time
