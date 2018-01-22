ceb8bcc328505f690787caf75bf9646271ef1652
TheRealPickle <therealpickle@gmail.com>
2018-01-21 21:03:09 -0700

    remove animations from cutscreens

--- states/movie_play.lua
+++ states/movie_play.lua
@@ -19,41 +19,41 @@ movie_play.movie_data = {
     IntertitleStep("We find Nikola Tesla\n\nnewly arrived in America,",2.5),
     IntertitleStep("working at the \n\nEdison Machine works.", 2.5),
     
-    -- Telsa walks up tp charles batchelor who is alread standing in the room
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 1,
-    }),
+    -- -- Telsa walks up tp charles batchelor who is alread standing in the room
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 1,
+    -- }),
     IntertitleStep("Mr. Batchelor,\n\nI've invented the\n\n24 standard machines.", 3),
     IntertitleStep("I'm here for my reward.", 2),
     
-    -- charles moves toward tesla
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
+    -- -- charles moves toward tesla
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
 
     IntertitleStep("Charles Batchelor:\n\nHa ha ha ha ha.\n\nThere was no reward.", 2),
     IntertitleStep("You FOOL.", 2),
     IntertitleStep("Thomas Alva Edison\n\nwill never pay.", 2),
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
-    IntertitleStep("Oh ... He WILL pay.", 2),
-    SequenceStep:new({
-      type = "animation",
-      run_time = 0.5,
-    }),
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
+    IntertitleStep("Mikola Tesla:\n\nOh ... He WILL pay.", 2),
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   run_time = 0.5,
+    -- }),
     IntertitleStep("And so will YOU", 2),
   },
   music = {track="figleaf", volume=0.3, offset=27},
@@ -62,120 +62,120 @@ movie_play.movie_data = {
 movie_play.movie_data2 = {
   sequence_steps = {
     -- Tesla walks into lab
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
-    IntertitleStep("Ahh. My Tesla\n\nExperimental Station.", 2),
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
+    IntertitleStep("Nikola Tesla:\n\nAhh. My Tesla\n\nExperimental Station.", 2),
     
-    -- Tesla walks over to to bench
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
+    -- -- Tesla walks over to to bench
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
     IntertitleStep("I wonder ...", 2),
     IntertitleStep("What signals will\n\nI find today?", 2.5),
     
-    -- tesls just stands there
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 4,
-    }),
+    -- -- tesls just stands there
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 4,
+    -- }),
     IntertitleStep("Eureka", 1),
     
-    -- tesls just stands there
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        -- player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 0.5,
-    }),
+    -- -- tesls just stands there
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     -- player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 0.5,
+    -- }),
     IntertitleStep("Martian Signals", 2),
     
     -- marconi walks in
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        -- player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     -- player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
     IntertitleStep("Guglielmo Marconi:\n\nHa ha ha.", 2),
     IntertitleStep("You Fool", 2),
     IntertitleStep("You just detected\n\nMY experiments ", 3),
     
-    -- Marconi runs away, tesla moves a bit toward the direction he left
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(-0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
-    IntertitleStep("You will answer for what\n\nyou've done, Marconi.", 2),
+    -- -- Marconi runs away, tesla moves a bit toward the direction he left
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(-0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
+    IntertitleStep("Nikola Telsa:\n\nYou will answer for what\n\nyou've done, Marconi.", 2),
   },
   music = {track="figleaf", volume=0.3, offset=27},
 }
 
 movie_play.movie_data3 = {
   sequence_steps = {
-    -- open to scene with 2 random guys facing edison
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        ---player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 1,
-    }),
-    IntertitleStep("Congratulations\n\nMr. Tesla", 2),
+    -- -- open to scene with 2 random guys facing edison
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     ---player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 1,
+    -- }),
+    IntertitleStep("Nobel Price Committee:\n\Congratulations\n\nMr. Tesla", 2),
     IntertitleStep("We'd like to award\n\nyou and Mr. Edison\n\n the Nobel Prize.", 2),
 
-    -- 2 guys leave
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        --player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 1,
-    }),
+    -- -- 2 guys leave
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     --player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 1,
+    -- }),
 
-    -- Edison walks in
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        --player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 3,
-    }),
+    -- -- Edison walks in
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     --player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 3,
+    -- }),
 
-    IntertitleStep("Edison: You'll have\n\n to kill me", 2),
-    IntertitleStep("before i ever share\n\n a nobel prize with you", 2),
+    IntertitleStep("Thomas Edison: You'll have\n\n to kill me", 2),
+    IntertitleStep("before i ever share\n\n a Nobel Prize with you", 2),
 
-    -- tesla walks forward, zoom in if you can
-    SequenceStep:new({
-      type = "animation",
-      start = function(self)
-        player:start_force_move(0.5, player.speed, 0)
-      end,
-      run_time = 2,
-    }),
-    IntertitleStep("So be it.", 2),
+    -- -- tesla walks forward, zoom in if you can
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   start = function(self)
+    --     player:start_force_move(0.5, player.speed, 0)
+    --   end,
+    --   run_time = 2,
+    -- }),
+    IntertitleStep("Nikola Telsa:\n\nSo be it.", 2),
 
     -- edison runs away
-    SequenceStep:new({
-      type = "animation",
-      run_time = 1,
-    }),
+    -- SequenceStep:new({
+    --   type = "animation",
+    --   run_time = 1,
+    -- }),
   },
   music = {track="figleaf", volume=0.3, offset=27},
 }
