275e2a3f3a6e4932a9de72557439319beb1a4187
jdeeny <jdeeny@gmail.com>
2018-01-23 20:07:10 -0700

    Randomize wall hp

--- map/tileset.lua
+++ map/tileset.lua
@@ -17,7 +17,7 @@ TileSet['opendoor']           = { function() return Tile:new('opendoor',        
 TileSet['fakedoor']           = { function() return Tile:new('fakedoor')           :setDoor(true):setSolid(true) end, }
 TileSet['floor']              = { function() return Tile:new('floor',           '-') end, }
 TileSet['teleporter']         = { function() return Tile:new('teleporter',      't') end, }
-TileSet['ballpost']           = { function() return Tile:new('ballpost',        'b'):setLayer(Layer.FURNITURE):setDestroyable('explodes', 'rubblefloor', 50) end,
+TileSet['ballpost']           = { function() return Tile:new('ballpost',        'b'):setLayer(Layer.FURNITURE):setDestroyable('explodes', 'rubblefloor', 20 + math.random(15)) end,
                                   function() return Tile:new('floor') end, }
 
 TileSet['water_border']       = { function() return Tile:new('water_border',    'w') end,
