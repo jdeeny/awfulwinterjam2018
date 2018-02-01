7a57ed6d52eb921f4a5db4dd47f0eb0b232ddce0
ontoclasm <yokomeshi@gmail.com>
2018-01-31 18:51:07 -0600

    Fix delays

--- delay.lua
+++ delay.lua
@@ -4,13 +4,13 @@ delay.events = {}
 
 function delay.start(time, f)
 	-- run function f after a delay
-	delay.events[game_time + time] = f
+	table.insert(delay.events, {end_time = game_time + time, f = f})
 end
 
 function delay.process()
 	for j,z in pairs(delay.events) do
-		if game_time > j then
-			z()
+		if game_time > z.end_time then
+			z.f()
 			delay.events[j] = nil
 		end
 	end
